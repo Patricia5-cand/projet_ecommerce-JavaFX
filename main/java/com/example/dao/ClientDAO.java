d87aaf8d3d117bdf53af867e53e92e608537d02d
fatima sidibe <fatima.sidibe@ism.edu.sn>
2026-04-15 17:40:06 +0000

    Implement ClientDAO with methods to get and save clients

--- main/java/com/example/dao/ClientDAO.java
+++ main/java/com/example/dao/ClientDAO.java
@@ -1,1 +1,37 @@
 
+package com.ecommerce.dao;
+
+import com.ecommerce.model.Client;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientDAO {
+    public List<Client> getAllClients() {
+        List<Client> clients = new ArrayList<>();
+        String query = "SELECT * FROM client";
+        try (Connection conn = DatabaseConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            while (rs.next()) {
+                clients.add(new Client(rs.getInt("id"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return clients;
+    }
+
+    public void saveClient(Client client) {
+        String query = "INSERT INTO client (nom, prenom, email) VALUES (?, ?, ?)";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, client.getNom());
+            pstmt.setString(2, client.getPrenom());
+            pstmt.setString(3, client.getEmail());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
