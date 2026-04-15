5a7b4ff56913ecf824830f5090d8396918f793c6
Patricia5-cand <165965539+Patricia5-cand@users.noreply.github.com>
2026-04-15 16:31:22 +0000

    Create Produit class for product representation
    Added a Produit class with attributes and methods for product management.

--- main/java/com/example/model/Produit.java
+++ main/java/com/example/model/Produit.java
@@ -1,1 +1,43 @@
+package com.model;
+
+public class Produit {
+    private int id;
+    private String nom;
+    private double prix;
+    private int stock;
+
+    public Produit() {}
+
+    public Produit(int id, String nom, double prix, int stock) {
+        this.id    = id;
+        this.nom   = nom;
+        this.prix  = prix;
+        this.stock = stock;
+    }
+
+    public Produit(String nom, double prix, int stock) {
+        this.nom   = nom;
+        this.prix  = prix;
+        this.stock = stock;
+    }
+
+    // Getters / Setters
+    public int getId()              { return id; }
+    public void setId(int id)       { this.id = id; }
+
+    public String getNom()              { return nom; }
+    public void setNom(String nom)      { this.nom = nom; }
+
+    public double getPrix()                 { return prix; }
+    public void setPrix(double prix)        { this.prix = prix; }
+
+    public int getStock()               { return stock; }
+    public void setStock(int stock)     { this.stock = stock; }
+
+    @Override
+    public String toString() {
+        return nom + " (" + String.format("%.2f", prix) + " FCFA)";
+    }
+}
+
 
